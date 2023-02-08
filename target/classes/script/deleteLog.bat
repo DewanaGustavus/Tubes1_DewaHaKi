8bfa1b2eaa77e6dd7f9644a269a3df963cb246d4
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-02-08 17:36:37 +0700

    Udah bisa nge blink

--- target/classes/script/deleteLog.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "d:\Coding\Tubes_and_Kerja\tubes\stima\Tubes1_DewaHaKi\src\main\java\script\"
-bash deleteLog.sh
-@REM apa ini wkwkwk
--- target/classes/script/deleteLog.sh
+++
@@ -1,2 +0,0 @@
-cd ../../../../../../stima/starter-pack/logger-publish
-rm -rf GameStateLog*
