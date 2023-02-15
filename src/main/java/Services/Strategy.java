84e9f1839430cfe05a530626cfa82d70e282351b
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2023-02-15 18:24:01 +0700

    Cuma rapihin

--- src/main/java/Services/Strategy.java
+++ src/main/java/Services/Strategy.java
@@ -21,21 +21,25 @@ public class Strategy {
     public static List<GameObject> strongEnemy;
     public static long startTime;
     
-
     public static void compute(BotService service, PlayerAction playerAction){
         startTime = System.nanoTime();
         extractBotService(service);
         extractGameObject();
 
         defaultAction(playerAction);
-        moveLogic(playerAction);
+        moveLogic(playerAction); // ! runFromBorder bad in late game
+        activateAfterBurnerLogic(playerAction); // ! not finished
+        deactivateAfterBurnerLogic(playerAction); // ! not finished
         fireTorpedoLogic(playerAction);
         fireTeleporterLogic(playerAction);
         teleportLogic(playerAction);
-        shieldLogic(playerAction);
+        shieldLogic(playerAction); // ! sometimes not open up
+        fireSupernovaLogic(playerAction); // ! not finished
+        detonateSupernovaLogic(playerAction); // ! not finished
         
-
-        // debugBotInfo(playerAction);
+        // debugBotInfo(playerAction, 1); // tick, execution time, bot info
+        // debugBotInfo(playerAction, 2); // bot inventory
+        // debugBotInfo(playerAction, 3); // enemy info
         service.setPlayerAction(playerAction);
     }
 
@@ -43,16 +47,8 @@ public class Strategy {
         return object != null && !object.isEmpty();
     }
 
-    private static boolean notEmpty(List<GameObject> object, int minSize){ // java gak ada default parameter jadi harus overload
-        return object != null && object.size() > minSize;
-    }
-
     private static boolean isEmpty(List<GameObject> object){
         return !notEmpty(object);
-    }
-
-    private static boolean isEmpty(List<GameObject> object, int minSize){
-        return !notEmpty(object, 1);
     }
     
     private static double getDistanceBetween(GameObject object1, GameObject object2) {
@@ -115,12 +111,13 @@ public class Strategy {
         weakEnemy = new ArrayList<GameObject>();
         strongEnemy = new ArrayList<GameObject>();
 		int weakEnemySizeOffset = 20;
-        if(isEmpty(objectList[1], 1))return;
-        for(int i=1;i<objectList[1].size();i++){
-            if(bot.size >= objectList[1].get(i).size + weakEnemySizeOffset){
-                weakEnemy.add(objectList[1].get(i));
+        if(isEmpty(objectList[1]))return;
+        objectList[1].remove(0); // remove ourself from list
+        for(GameObject player : objectList[1]){
+            if(bot.size >= player.size + weakEnemySizeOffset){
+                weakEnemy.add(player);
             }else{
-                strongEnemy.add(objectList[1].get(i));
+                strongEnemy.add(player);
             }
         }
     }
@@ -182,48 +179,19 @@ public class Strategy {
     }
 
     public static void fireTorpedoLogic(PlayerAction playerAction){
-        /*** 
-        int angle1=30;
-        if(!isEmpty(objectList[6]))angle1 = Math.abs(objectList[6].get(0).currentHeading - ((getHeadingBetween(objectList[6].get(0)) + 180) % 360));
-        if (bot.size > 20 && getDistanceBetween(bot, objectList[1].get(0)) < 450 && bot.TorpedoSalvoCount > 4) {
-            playerAction.action = PlayerActions.FIRETORPEDOES;
-            playerAction.heading = getHeadingBetween(objectList[1].get(0));
-            System.out.println("Firing torpedoes!!");
-        } 
-        else if (bot.size > 20 && !isEmpty(objectList[6]) && angle1<=20 && getDistanceBetween(bot, objectList[6].get(0)) < 1.2 * bot.size && bot.TorpedoSalvoCount > 3){
-            playerAction.action = PlayerActions.FIRETORPEDOES;
-            playerAction.heading = getHeadingBetween(objectList[6].get(0));
-            System.out.println("Firing torpedoes for defense :)))");
-        }
-
-        int minimumSize = 50;
-        double distLowerBound = 200;
-        int fireChance = random.nextInt(100);
-        if(bot.size>minimumSize && bot.TorpedoSalvoCount>2 && !isEmpty(objectList[1])){
-        for(int i=1;i<objectList[1].size();i++){
-            double dist = getDistanceBetween(bot, objectList[1].get(i)) - objectList[1].get(i).size-bot.size;
-            if(dist < distLowerBound){
-            playerAction.action = PlayerActions.FIRETORPEDOES;
-            playerAction.heading = getHeadingBetween(objectList[1].get(i));
-            break;
-            }
-            
-        }
-        }
-        ***/
         int minimumSize = 50;
         double distLowerBound = 600;
         int fireChance = random.nextInt(100);
         if(bot.TorpedoSalvoCount >= 3)fireChance = random.nextInt(40, 100); // maximum is 5, dont waste
-        if(bot.TorpedoSalvoCount >= 2 && getDistanceBetween(bot, objectList[1].get(1))<100)fireChance = random.nextInt(75, 100);
-        if(bot.size < minimumSize || fireChance < 97 || isEmpty(objectList[1], 1))return;
-        for(int i=1;i<objectList[1].size();i++){
-            double dist = getDistanceBetween(bot, objectList[1].get(i)) - objectList[1].get(i).size;
+        if(bot.TorpedoSalvoCount >= 2 && getShortestObjectListDistance(objectList[1])<100)fireChance = random.nextInt(75, 100);
+        if(bot.size < minimumSize || fireChance < 97 || isEmpty(objectList[1]))return;
+        for(GameObject player : objectList[1]){
+            double dist = getDistanceBetween(bot, player) - player.size;
             if(dist >= distLowerBound){
                 break;
             }
             playerAction.action = PlayerActions.FIRETORPEDOES;
-            playerAction.heading = getHeadingBetween(objectList[1].get(i));
+            playerAction.heading = getHeadingBetween(player);
             break;
         }
     }
@@ -238,9 +206,7 @@ public class Strategy {
         double weakSizeMultiplier = 0.9;
         GameObject target = null;
         for(GameObject enemy : weakEnemy){
-            // System.out.println((bot.size - 20) * weakSizeMultiplier);
             if(enemy.size > (bot.size - 20) * weakSizeMultiplier)continue;
-            
             if(getDistanceBetween(bot, enemy) <= distLowerBound){
                 target = enemy;
                 break;
@@ -277,11 +243,7 @@ public class Strategy {
         for(GameObject torpedo : objectList[6]){
             double dist = getDistanceBetween(bot, torpedo) - bot.size - torpedo.size;
             int angle1 = Math.abs(torpedo.currentHeading - ((getHeadingBetween(torpedo) + 180) % 360));
-            // System.out.printf("dist : %d\n", (int)dist);
-            // System.out.printf("dist : %d, angle : %d\n", (int)dist, angle1);
             if(dist > distLowerBound || (angle1 > toleratedAngle && angle1 < (360-toleratedAngle)) )continue;
-            // int angle2 = Math.abs(torpedo.currentHeading - getHeadingBetween(torpedo));
-            // System.out.printf("angel : %d %d\n", angle1, angle2);
             useShield = true;
             break;
         }
@@ -289,19 +251,46 @@ public class Strategy {
         if(useShield)playerAction.action = PlayerActions.ACTIVATESHIELD;
     }
 
+    public static void activateAfterBurnerLogic(PlayerAction playerAction){
+            
+    }
+
+    public static void deactivateAfterBurnerLogic(PlayerAction playerAction){
+        
+    }
+
+    public static void fireSupernovaLogic(PlayerAction playerAction){
+        
+    }
+    
+    public static void detonateSupernovaLogic(PlayerAction playerAction){
+        
+    }
+
+
     public static void runFromBorder(PlayerAction playerAction){
         double distLowerBound = 20 ;
         if(world.getCurrentTick()!=null)distLowerBound+=world.getCurrentTick()/5;
         if(world == null || world.getRadius() == null)return;
         double dist = world.radius - getDistanceBetween(bot.getPosition(), world.centerPoint) - bot.size;
-        // System.out.printf("%d %d %d %d\n", world.radius, bot.getPosition().x, bot.getPosition().y, bot.size);
         if(dist > distLowerBound)return;
         playerAction.heading = getHeadingBetween(world.centerPoint);
-        System.out.printf("gocenter");
     }
 
-    public static void debugBotInfo(PlayerAction playerAction){
-        if(notEmpty(objectList[1])){
+    public static int[] getPlayerInfo(GameObject player){
+        int info[] = {player.size, player.TorpedoSalvoCount, player.ShieldCount, player.TeleporterCount, player.SupernovaAvailable};
+        return info;
+    }
+
+    public static void debugBotInfo(PlayerAction playerAction, int debugType){
+        /*
+        debugType :
+        1. tick, execution time, bot info
+        2. bot inventory
+        3. enemy info
+        */ 
+        if(gameState.getWorld().getCurrentTick() == null)return;
+        if(debugType == 1){
             System.out.printf("Tick : %d\n", world.getCurrentTick());
             long endTime = System.nanoTime();
             long executionTime = (endTime - startTime) / 1000;
@@ -309,7 +298,28 @@ public class Strategy {
             System.out.printf("Size : %d\n", bot.size);
             System.out.printf("Position : %d %d\n", bot.getPosition().x, bot.getPosition().y);
             System.out.println("Action : " + playerAction.action);
-            System.out.printf("Heading : %d\n\n", playerAction.heading);
+            System.out.printf("Heading : %d\n", playerAction.heading);
+            System.out.println();
+        }else if(debugType == 2){
+            System.out.printf("Tick : %d\n", world.getCurrentTick());
+            System.out.printf("Size : %d\n", bot.size);
+            System.out.println("Action : " + playerAction.action);
+            System.out.printf("Torpedo : %d\n", bot.TorpedoSalvoCount);
+            System.out.printf("Teleport : %d\n", bot.TeleporterCount);
+            System.out.printf("Shield : %d\n", bot.ShieldCount);
+            System.out.printf("Supernova : %d\n", bot.SupernovaAvailable);
+            System.out.println();
+        }else if(debugType == 3){
+            System.out.printf("Tick : %d\n", world.getCurrentTick());
+            System.out.printf("Size : %d\n", bot.size);
+            System.out.printf("Position : %d %d\n", bot.getPosition().x, bot.getPosition().y);
+            System.out.println("Action : " + playerAction.action);
+            System.out.println("Enemy info : [Size, Torpedo, Shield, Teleport, Supernova]");
+            for(GameObject player : objectList[1]){
+                int playerInfo[] = getPlayerInfo(player);
+                System.out.println(Arrays.toString(playerInfo));
+            }
+            System.out.println();
         }
     }
 
